6416dd5a1058de795af2f6a2642bbc35d3c7af56
Florencia-Mussis <mussisflorencia@gmail.com>
2024-07-01 18:12:43 +0200

    Proyecto final CH

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// MENU MOBILE 
+// MOBILE MENU
 const toggle = document.getElementById("toggle")
 const sidebar = document.getElementById("sidebar")
 
 document.onclick = function (e) {
     if (e.target.id !== "sidebar" && e.target.id !== "toggle") {
-        toggle.classList.remove("active");
+        toggle.classList.remove("active")
         sidebar.classList.remove("active")
     }
 }
@@ -14,109 +14,206 @@ toggle.onclick = function () {
     sidebar.classList.toggle("active")
 }
 
-// FUNCIONES VALIDAR DATOS PERSONALES Y CALCULAR PRESTAMO 
-const mensajeError = document.getElementById("error")
-const spanCalculo = document.querySelector("#resultado span")
-const btnVerUltimoCalculo = document.querySelector("#verUltimoCalculo")
+// FUNCTIONS VALIDATE PERSONAL DATA AND CALCULATE LOAN 
+const spanCalculation = document.querySelector("#seeLastCalculation span")
+const btnseeLastCalculation = document.querySelector("#seeLastCalculation")
 const interestRate = 8 / 100
+const animation = "<img src='assets/imagenes/dots.gif' >"
+const buttonCalculate = document.getElementById("button-calculate")
+const URLtestimonials =  "js/testimonials.json"
 
-function validarDatos() {
-    const name = document.getElementById("Name").value
-    const surname = document.getElementById("Surname").value
-    const emailAddress = document.getElementById("email").value
-    const amountWithCurrency = document.getElementById("amount").value;
-    const amount = parseFloat(amountWithCurrency.slice(2));
-    const months = document.getElementById("months").value;
+// USER DATA STRUCTURE
+const userData = {
+    name: '',
+    surname: '',
+    emailAddress: '',
+    amount: 0,
+    months: 0
+}
 
-    const nameOk = checkName(name)
-    const surnameOk = checkSurname(surname)
-    const emailAddressOk = checkEmailAddress(emailAddress)
+// CALCULATIONS ARRAY
+const calculations = []
 
-    if (nameOk && surnameOk && emailAddressOk && amount && months) {
-        deleteError()
-        mostrarResultado(amount, months)
+function showAlert(message, icon) {
+    Swal.fire({
+        text: message,
+        icon: icon,
+        timer: 4000,
+        showConfirmButton: false,
+        color: "#0A2864",
+        html: message.replace(/\n/g, '<br>')
+    })
+}
+
+function validateData() {
+    userData.name = document.getElementById("Name").value
+    userData.surname = document.getElementById("Surname").value
+    userData.emailAddress = document.getElementById("email").value
+    const amountWithCurrency = document.getElementById("amount").value
+    userData.amount = parseFloat(amountWithCurrency.slice(2))
+    userData.months = document.getElementById("months").value
+
+    const nameOk = checkName(userData.name)
+    const surnameOk = checkSurname(userData.surname)
+    const emailAddressOk = checkEmailAddress(userData.emailAddress)
+
+    if (nameOk && surnameOk && emailAddressOk && userData.amount && userData.months) {
+        showResult(userData.amount, userData.months)
+    } else if (!nameOk && !surnameOk && !emailAddressOk && !userData.amount && !userData.months){
+        showAlert("Ingrese los datos solicitados para calcular su préstamo.", "warning")
     } else {
         if (!nameOk) {
-            mensajeError.textContent = " ❌ Ingrese un nombre válido."
+            showAlert("Ingrese un nombre válido.", "warning")
         }
         if (!surnameOk) {
-            mensajeError.textContent = " ❌ Ingrese un apellido válido."
+            showAlert("Ingrese un apellido válido.", "warning")
         }
         if (!emailAddressOk) {
-            mensajeError.textContent = " ❌ Ingrese un correo electronico válido"
+            showAlert("Ingrese un correo electronico válido.", "warning")
         }
-        if (!amount || !months ) {
-            mensajeError.textContent = " ❌ Ingrese los valores correctamente"
+        if (!userData.amount || !userData.months ) {
+            showAlert("Ingrese los valores correctamente.", "warning")
         }
     }
 }    
 
 function checkName(name) {
-    const regexName = /^[a-zA-Z\s]+$/;
-    return regexName.test(name);
+    const regexName = /^[a-zA-Z\s]+$/
+    return regexName.test(name)
 }
 
 function checkSurname(surname) {
-    const regexSurname = /^[a-zA-Z\s]+$/;
-    return regexSurname.test(surname);
+    const regexSurname = /^[a-zA-Z\s]+$/
+    return regexSurname.test(surname)
 }
 
 function checkEmailAddress(emailAddress) {
-    const regexEmailAddress = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regexEmailAddress.test(emailAddress);
+    const regexEmailAddress = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return regexEmailAddress.test(emailAddress)
 }
 
-function calcularPrestamo(amount, months) {
+function calculateLoan(amount, months) {
     if (amount && months) {
-        const periods = months / 12;
-        let result = amount * (1 + interestRate) ** periods;
-        result /= months;
+        const periods = months / 12
+        let result = amount * (1 + interestRate) ** periods
+        result /= months
         return result.toFixed(2)
     } else {
         return null
     }
 }
 
-function mostrarResultado(amount, months) {
-    let resultado =  calcularPrestamo(amount, months)
+function showResult(amount, months) {
+    let result =  calculateLoan(amount, months)
+    buttonCalculate.innerHTML = animation
+    
+    setTimeout(() => {
+        buttonCalculate.textContent = "CALCULAR PRÉSTAMO"
+        if (result !== null) {
+            showAlert("Cuota mensual calculada exitosamente.", "success")
+            let seeResult =  document.getElementById("result")
 
-    if (resultado !== null) {
-        let verResultado =  document.getElementById("resultado")
+            if ( seeResult === null) {
+                seeResult =  document.createElement("div")
+                seeResult.id = "result"
+                document.querySelector(".load-section-container").appendChild(seeResult)
+            } 
+            
+            seeResult.innerHTML = `Su cuota mensual es de <span>€ ${result}</span>`
 
-        if ( verResultado === null) {
-            let verResultado =  document.createElement("div")
-            verResultado.id = "resultado"
-            document.querySelector(".load-section-container").appendChild(verResultado);
-            verResultado.innerHTML = `Tu cuota mensual es de <span>€ ${resultado}</span>`;
-        } else {
-            verResultado.innerHTML = `Tu cuota mensual es de <span>€ ${resultado}</span>`;
+            const contractContainer = document.createElement("div")
+            const contract = document.createElement("button")
+            contract.id = "button-contract"
+            contract.textContent = "CONTRATAR"
+
+            contractContainer.appendChild(contract)
+            seeResult.appendChild(contractContainer)
+
+            contract.addEventListener("click", () => {
+                showAlert("Su solicitud ha sido procesada.\nEn breve recibirá un correo electrónico con todos los detalles.", "success")
+            })
+           
+            saveLastCalculationLS(amount, months,result)
         }
-        guardarUltimoCalculoLS(resultado);
-    }
+    }, 1500)
 }
 
-function deleteError() {
-    mensajeError.textContent = ""
-}
-
-function mostrarUltimoCalculo() {
-    return JSON.parse(localStorage.getItem("ultimoCalculo"))
+function showLastCalculation() {
+    return JSON.parse(localStorage.getItem("lastCalculation"))
 } 
 
-function guardarUltimoCalculoLS(resultado) {
-    const ultimoCalculo = {
+function saveLastCalculationLS(amount, months,result) {
+    const lastCalculation = {
         fecha: new Date(),
-        resultadoCalculo: resultado
+        amountCalculo: amount,
+        monthsCalculo: months,
+        resultCalculation: result
     }
-    localStorage.setItem("ultimoCalculo", JSON.stringify(ultimoCalculo))
+    calculations.push(lastCalculation)
+    localStorage.setItem("lastCalculation", JSON.stringify(lastCalculation))
 }
 
-btnVerUltimoCalculo.addEventListener("click", () => {
-    const ultimoCalculo = mostrarUltimoCalculo()
+btnseeLastCalculation.addEventListener("click", () => {
+    const lastCalculation = showLastCalculation()
     
-    if (ultimoCalculo !== null) {
-        alert("Último cálculo realizado el " + new Date(ultimoCalculo.fecha).toLocaleString() + " con una cuota mensual de € " + ultimoCalculo.resultadoCalculo);
+    if (lastCalculation !== null) {
+        showAlert(
+            "Último cálculo realizado el día " + new Date(lastCalculation.fecha).toLocaleString() +
+            "\n\nDinero solicitado: " + lastCalculation.amountCalculo +  " €" +
+            "\nPlazo de devolución: " + lastCalculation.monthsCalculo + " meses" + 
+            "\n\nCuota mensual de " + lastCalculation.resultCalculation + " €" , "info"
+        ) 
     } else {
-        alert("No hay cálculos previos.");
+        showAlert("No hay cálculos previos.", "info")
     }
 })
+
+function uploadTestimonials(testimonials) {
+    if (testimonials.length > 0) {
+        const testimonialsSection = document.querySelector("#testimonials-section")
+        const testimonialsContainer = document.createElement("div")
+        testimonialsContainer.classList.add("testimonials-container")
+        testimonialsSection.appendChild(testimonialsContainer)
+
+        testimonials.forEach((testimonial) => {
+            const testimonialElement = document.createElement("div");
+            testimonialElement.classList.add("testimonial")
+            testimonialElement.innerHTML = `
+                <div>
+                    <p>${"⭐".repeat(testimonial.rating)}</p>
+                    <p class="name"><strong>${testimonial.author}
+                    </strong></p>
+                    <p><strong>(${testimonial.occupation})</strong></p>
+                </div>
+                <div>
+                    <p>${testimonial.message}</p>
+                </div>
+            `;
+            testimonialsContainer.appendChild(testimonialElement)
+        })
+    }
+}
+
+// function getTestimonials() {
+//     fetch(URLtestimonials)
+//     .then((response) => response.json())
+//     .then((data) => uploadTestimonials(data))
+//     .catch((error) => {
+//         console.error("Error al cargar los testimonios", error);
+//     })
+// }
+
+async function getTestimonials() {
+    try {
+        const response = await fetch(URLtestimonials)
+        const data = await response.json()
+        uploadTestimonials(data)
+    } catch(error) {
+        const link = document.querySelector(".testimonials-link")
+        link.addEventListener("click", () => {
+         showAlert("Error al cargar los testimonios, intente más tarde.", "error")
+        })
+    }    
+}
+
+getTestimonials()
